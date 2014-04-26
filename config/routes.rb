c4b0e773ee4b3e3e15b1f60d3a58065cc86ca8f0
Saurabh Malode <saurabh_malode@yahoo.co.in>
2014-04-26 19:38:12 +0530

    finished signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Pdamwebapp::Application.routes.draw do
   resources :users 
   root  'static_pages#home'
+  resources :sessions, only: [:new, :create, :destroy]
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   
