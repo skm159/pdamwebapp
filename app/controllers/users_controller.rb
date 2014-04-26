5713f3953bc3f51c2398eee5c914b86b9418d1a6
Saurabh Malode <saurabh_malode@yahoo.co.in>
2014-04-26 16:53:35 +0530

    Test successful after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-     flash[:success] = "Welcome to the Sample App!"
+     flash[:success] = "Welcome to the PDAM Webapp!"
      redirect_to @user 
     else 
       render 'new'
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,24 +4,32 @@ describe "User pages" do
 
   subject { page }
 
-  describe "signup page" do
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
+
+  describe "signup" do
+
     before { visit signup_path }
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
-  end
-  
-  describe "profiles page" do
-    let(:user) { FactoryGirl.create(:user) }
-    before {visit user_path(user)}
-    it {should have_content(user.name)}
-    it {should have_title(user.name)}
-  end
-  
-  describe "with valid information" do
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
+        fill_in "Location",     with: "Nagpur"
+        fill_in "Role",         with: "Soft engg"
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
@@ -30,4 +38,5 @@ describe "User pages" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
     end
+  end
 end
